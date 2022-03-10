3594d0809468582b359ed154243a645de482e017
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-10 22:24:18 +0600

    Chessboard

---
+++ Chessboard/chess.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Chessboard Create</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<div class="chess">
+			<form method="POST">
+				<input type="text" name="num" placeholder="Enter The Nubmer">
+				<input class="btn" type="submit" name="submit" value="Generate Chessoard" required>
+			</form>
+
+			<br>
+
+			<table>
+				<?php
+					if (isset($_POST['submit'])){
+
+						$inp = $_POST['num'];
+						for ($row=1; $row<=$inp ; $row++) { 
+							echo "<tr>";
+							for ($col=1; $col<=$inp ; $col++)
+								if (($row+$col)%2==0) {
+									echo "<td class='bgBlack'></td>";
+								}else{
+									echo "<td></td>";
+								}
+
+							echo "</tr>";
+						}
+					}
+				?>
+			</table>
+		</div>
+	</body>
+</html>
