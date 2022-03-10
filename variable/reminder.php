5b701290b33a45a73e14554097517c927299fb64
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-10 22:33:34 +0600

    variable

---
+++ variable/reminder.php
@@ -0,0 +1,16 @@
+<?php
+
+	$number = 26;
+	$dividor = 3;
+	$rem = $number % $dividor;
+
+	echo "Reminder : $rem </br></br>";
+
+	$base = 4;
+	$pow = 3;
+	
+	$powOfNum = pow($base , $pow);
+
+	echo "$base to the power $pow is : $powOfNum </br></br>";
+
+?>
---
+++ variable/variable.php
@@ -0,0 +1,23 @@
+<?php
+
+	echo "Variable<br>";
+
+	$name = "Nurul Islam";
+	$age = 24;
+
+	$number = 5;
+	$number2=5;
+	$sum = $number + $number2;
+
+	echo "$name is $age years of old<br>";
+
+	$num1 = 2.5;
+	$num2=5.5;
+	$sum1 = $num1 + $num2;
+	echo "Sum of the Number : $sum1<br>";
+
+	echo "$name is $age years of old<br>";
+
+	
+
+?>
