fafc5133acc7931ce92c6280055192911c4e1efa
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-10 22:33:01 +0600

    rule

---
+++ structure/structure.php
@@ -0,0 +1,37 @@
+<?php
+
+    echo "Nurul Islam<br>";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Structure </title>
+</head>
+	<body>
+
+		<?php
+
+			echo "PHP script can put anywhere in php file <br>";
+			echo "Only u have to do the thing is close the php tag";
+
+		?>
+
+		<h1>HTML in .php file (its possible).</h1>
+
+	</body>
+</html>
+
+
+<?php
+
+	// This wont be print 
+	#This wont be print 
+
+		/*this 
+		also won't*/
+
+		echo "See the src code you will get to know how to comment in php<br>";
+
+ ?>
