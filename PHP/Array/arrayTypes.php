ffa7f2822ef4a943a2f164d912f7f7dedaf8ac99
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-03-10 22:21:17 +0600

    Php

---
+++ PHP/Array/arrayTypes.php
@@ -0,0 +1,7 @@
+<!-- 
+	Array : Special types of variable can holds more than one value at a time. or
+
+			Can be used to store linear data of similar types.
+ -->
+
+ 
---
+++ PHP/Array/forEach.php
@@ -0,0 +1,13 @@
+<?php
+	
+	$names = array("Abc" ,"ACB","MNO");
+	
+
+	echo "<ol>";
+	foreach ($names as $value) {
+		
+		echo "<li>". $value ."</li>";
+	}
+
+	echo "</ol>";
+?>
